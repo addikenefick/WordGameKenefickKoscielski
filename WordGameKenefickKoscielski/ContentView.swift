0bb444302971d8922f6032a086cdfcb29e5e2e7c
Addison Kenefick <addisonkenefick@gmial.com>
2026-02-19 07:57:03 -0600

    Initial Commit

---
+++ WordGameKenefickKoscielski/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordGameKenefickKoscielski
+//
+//  Created by ADDISON KENEFICK on 2/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordGameKenefickKoscielski/WordGameKenefickKoscielskiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameKenefickKoscielskiApp.swift
+//  WordGameKenefickKoscielski
+//
+//  Created by ADDISON KENEFICK on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGameKenefickKoscielskiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
