4b5614b6b6d3bb32e6270535f408eefb7f2c21bb
Sammy Koscielski <samantha.koscielski27@d155.org>
2026-02-20 08:10:33 -0600

    Working on getting API in

---
+++ WordGameKenefickKoscielski/GameView.swift
@@ -0,0 +1,25 @@
+//
+//  GameView.swift
+//  WordGameKenefickKoscielski
+//
+//  Created by SAMANTHA KOSCIELSKI on 2/20/26.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        
+    }
+    
+    func getDictionary() {
+        
+        let session = URLSession.shared
+        
+        
+    }
+}
+
+#Preview {
+    GameView()
+}
