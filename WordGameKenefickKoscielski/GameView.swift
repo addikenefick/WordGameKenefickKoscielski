24d9c2ed94f8a1414624fe99b911c2e6a1f098e3
Sammy Koscielski <samantha.koscielski27@d155.org>
2026-02-23 08:28:43 -0600

    Finished adding dictionary api

--- WordGameKenefickKoscielski/GameView.swift
+++ WordGameKenefickKoscielski/GameView.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 struct GameView: View {
     var body: some View {
-        
+        VStack {
+            
+        }
+        .onAppear() {
+            getDictionary()
+        }
     }
-    
+        
     func getDictionary() {
         
         let session = URLSession.shared
         
+        let dictionaryURL = URL(string: "https://od-api-sandbox.oxforddictionaries.com/api/v2&apikey=8a8d9850e91f06af0839113f83a62dda")!
         
+        let dataTask = session.dataTask(with: dictionaryURL) {
+            (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let error = error {
+                print("Error: \(error)")
+            } else {
+                if let data = data {
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: data) as? NSDictionary {
+                        print(jsonObj)
+                    } else {
+                        print("Error: unable to convert json object")
+                    }
+                } else {
+                    print("Error: did not receive data")
+                }
+            }
+        }
+        dataTask.resume()
     }
 }
 
